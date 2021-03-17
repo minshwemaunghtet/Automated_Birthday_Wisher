d2bede8081f0b291f9b150aca489ac40030aa1df
Min Shwe Maung Htet <54788740+minshwemaunghtet@users.noreply.github.com>
2021-03-16 19:11:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ data = pandas.read_csv("birthdays.csv")
 birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
 if today_tuple in birthdays_dict:
     birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
+    file_path = f"letter_{random.randint(1,3)}.txt"
     with open(file_path) as letter_file:
         contents = letter_file.read()
         contents = contents.replace("[NAME]", birthday_person["name"])
